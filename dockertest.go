19fb21e635907bd06c0f05b343181d823a7ad5e0
Hysios Hu <hysios@gmail.com>
2021-04-18 14:57:51 +0800

    First commit

---
+++ dockertest.go
@@ -0,0 +1,67 @@
+package dockertest
+
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+var (
+	Dockerfiles = []string{
+		"docker-compose.yml",
+		"docker-compose.yaml",
+	}
+)
+
+func Prepare() error {
+	dir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	list, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+
+	var found string
+
+	for _, fi := range list {
+		for _, s := range Dockerfiles {
+			if fi.Name() == s && !fi.IsDir() {
+				found = fi.Name()
+				goto Found
+			}
+		}
+	}
+
+	return errors.New("not found dockerfiles")
+
+Found:
+	if IsDockerRunning() {
+		return errors.New("always running")
+	}
+
+	return nil
+}
+
+func IsDockerRunning() bool {
+	b, err := exec.Command("docker-compose", "ps", "-q", "|", "wc", "-l").Output()
+	if err != nil {
+		return false
+	}
+
+	n, err := strconv.Atoi(strings.TrimSpace(string(b)))
+	if err != nil {
+		return false
+	}
+
+	return n > 0
+}
+
+func RunDockerCompose() error {
+	return exec.Command("docker-compose", "-d", "up").Run()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hysios/dockertest
+
+go 1.15
